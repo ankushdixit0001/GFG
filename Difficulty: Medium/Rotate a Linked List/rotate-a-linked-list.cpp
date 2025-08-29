0afdfed6a375fbe82d1c26114b1ea7fb080d9514
Ankush Dixit <ankushdixit29@gmail.com>
2025-08-29 13:54:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.cpp
+++ Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.cpp
@@ -1,109 +1,45 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-
-// } Driver Code Ends
 /*
-
-struct Node {
+class Node {
     int data;
-    struct Node *next;
+    Node* next;
+
     Node(int x) {
         data = x;
         next = NULL;
     }
 };
-
 */
 
 class Solution {
   public:
     Node* rotate(Node* head, int k) {
-        if(k==0){
-            return head;
-        }
-        int len=1;
-        Node* temp=head;
-        while(temp->next){
-            temp=temp->next;
+        if (!head || k == 0) return head;
+    
+        // Step 1: Find length and tail
+        Node* tail = head;
+        int len = 1;
+        while (tail->next) {
+            tail = tail->next;
             len++;
         }
-        k=k%len;
-        if(k==0){
-            return head;
+    
+        // Step 2: Normalize k
+        k = k % len;
+        if (k == 0) return head;
+    
+        // Step 3: Make list circular
+        tail->next = head;
+    
+        // Step 4: Move to new tail (len - k steps for right rotation)
+        Node* newTail = head;
+        for (int i = 1; i < k; i++) {
+            newTail = newTail->next;
         }
-        temp->next=head;
-        Node* newtail=head;
-        for(int i=1;i<k;i++){
-            newtail=newtail->next;
-        }    
-        Node* newhead=newtail->next;
-        newtail->next=nullptr;
-        return newhead;
+    
+        // Step 5: Break the circle
+        Node* newHead = newTail->next;
+        newTail->next = nullptr;
+    
+        return newHead;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-
-        // Read numbers from the input line
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Node* head = nullptr;
-
-        // Check if the array is empty
-        if (!arr.empty()) {
-            head = new Node(arr[0]);
-            Node* tail = head;
-            for (size_t i = 1; i < arr.size(); ++i) {
-                tail->next = new Node(arr[i]);
-                tail = tail->next;
-            }
-        }
-        int k;
-        cin >> k;
-        cin.ignore();
-
-        Solution ob;
-        head = ob.rotate(head, k);
-        printList(head);
-        cout << "~\n";
-    }
-    return 1;
-}
-
-// } Driver Code Ends
