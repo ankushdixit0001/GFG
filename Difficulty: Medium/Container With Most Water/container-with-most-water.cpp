44adde5d9a22a1869cb9cd5a23251ecd9e41b297
Ankush Dixit <ankushdixit29@gmail.com>
2025-08-31 13:58:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -1,55 +1,27 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     int maxWater(vector<int> &arr) {
         // code here
-        int n = arr.size();
-        int left=0, right=n-1, Area =0 , ans = 0;
-        while (left<=right){
-
-            int breath = right-left;
-            Area = breath*min(arr[left] , arr[right]);
-            ans = max(ans , Area);
-            if(arr[left]<arr[right]){
-            left++;
-            }
-            else{
+        int n=arr.size();
+        int left=0, right=n-1, ans=0, Area=0;
+        
+        //Time Complexity: O(n) — each element is visited at most once.
+        //Space Complexity: O(1) — constant extra space
+        
+        while (left < right) {
+            int height = min(arr[left], arr[right]);
+            int breadth = right - left;
+            Area = height * breadth;
+            ans = max(ans, Area);
+        
+            // Move the pointer pointing to the shorter line
+            if (arr[left] < arr[right]) {
+                left++;
+            } else {
                 right--;
             }
         }
+        
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.maxWater(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
