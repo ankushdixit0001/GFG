e8604c8d36043b097103ea5aec1e6e74d73c35a6
Ankush Dixit <ankushdixit29@gmail.com>
2025-09-01 22:54:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -0,0 +1,59 @@
+/* Node structure  used in the program
+
+struct Node{
+    int data;
+    struct Node * next;
+    struct Node * bottom;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    Node* merge(Node* temp1,Node* temp2){
+        Node* dummy=new Node(-1);
+        Node* res=dummy;
+        
+        while(temp1&&temp2){
+            if(temp1->data<temp2->data){
+                res->bottom=temp1;
+                res=temp1;
+                temp1=temp1->bottom;
+            } else {
+                res->bottom=temp2;
+                res=temp2;
+                temp2=temp2->bottom;
+            }
+            res->next=NULL;
+        }
+        //attach remaining nodes
+        res->bottom = temp1 ? temp1 : temp2;
+        
+        return dummy->bottom;
+    }
+    
+    Node* helper(Node* head){
+        if(!head||!head->next){
+            return head;
+        }
+        
+        Node* mergeHead=helper(head->next);
+        
+        return merge(head,mergeHead);
+    }
+    
+    Node *flatten(Node *root) {
+        // Your code here
+        if(!root||!root->next){
+            return root;
+        }
+        
+        return helper(root);
+    }
+};
