7fa5e0a3ffd7fd8340a34c8bcb200823a5572d05
Ankush Dixit <ankushdixit29@gmail.com>
2025-09-08 19:22:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.cpp
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.cpp
@@ -1,61 +1,50 @@
-/* class Node { 
-    public: 
-        int data; 
-        Node* next;
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
     Node(int x){
         data = x;
         next = NULL;
     }
-}; 
-*/ 
-class Solution { 
-    public: 
-    Node* findMiddle(Node* head){ 
-        Node* slow=head; 
-        Node* fast=head->next; 
+};
+*/
+class Solution {
+  public:
+    Node* findMiddle(Node* head){
+        Node* slow=head;
+        Node* fast=head->next;
         while(fast&&fast->next){
-            slow=slow->next; 
+            slow=slow->next;
             fast=fast->next->next;
-        } 
-        return slow; 
-    } 
-    
-    Node* merge(Node* leftHead,Node* rightHead){ 
-        Node* dummyNode=new Node(-1); 
-        Node* temp=dummyNode;
-        while(leftHead&&rightHead){
-            if(leftHead->data<rightHead->data){
-                temp->next=leftHead;
-                leftHead=leftHead->next;
-            } else {
-                temp->next=rightHead;
-                rightHead=rightHead->next;
-            }
-            temp=temp->next;
         }
-        temp->next=leftHead?leftHead:rightHead;
-    
-        Node* result = dummyNode->next;
-        delete dummyNode;
-        return result;
+        return slow;
     }
     
-    Node* mergeSortHelper(Node* head) {
-        // code here
-        if(!head||!head->next){
-            return head;
+    Node* merge(Node* leftHead, Node* rightHead){
+        if(!leftHead) return rightHead;
+        if(!rightHead) return leftHead;
+        
+        Node* dummyNode=new Node(-1);
+        Node* curr=dummyNode;
+        
+        while(leftHead&&rightHead){
+            if(leftHead->data<rightHead->data){
+                curr->next=leftHead;
+                leftHead=leftHead->next;
+            } else {
+                curr->next=rightHead;
+                rightHead=rightHead->next;
+            }
+            curr=curr->next;
         }
-    
-        Node* middle=findMiddle(head);
-        Node* left=head;
-        Node* right=middle->next;
-        middle->next=NULL;
-    
-        left=mergeSortHelper(left);
-        right=mergeSortHelper(right);
-    
-        return merge(left,right);
-    
+        curr->next=leftHead?leftHead:rightHead;
+        
+        Node* result=dummyNode->next;
+        delete dummyNode;
+        
+        return result;
     }
     
     Node* mergeSort(Node* head) {
@@ -63,9 +52,17 @@ class Solution {
         if(!head||!head->next){
             return head;
         }
-    
-        return mergeSortHelper(head);
-    
+        
+        Node* middle=findMiddle(head);
+        
+        Node* left=head;
+        Node* right=middle->next;
+        middle->next=nullptr;
+        
+        left=mergeSort(left);
+        right=mergeSort(right);
+        
+        return merge(left,right);
+        
     }
-
 };
