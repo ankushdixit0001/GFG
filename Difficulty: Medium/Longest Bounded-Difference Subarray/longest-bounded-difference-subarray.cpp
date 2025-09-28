cd151226376eadc4f2315ee02333057a39dbdb05
Ankush Dixit <ankushdixit29@gmail.com>
2025-09-28 22:26:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.cpp
+++ Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.cpp
@@ -1,71 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> longestSubarray(vector<int>& arr, int x) {
-        //Code here
-        deque<int>mindq,maxdq;
-        int s=0,e=0;
-        int ansStart=0,ansEnd=0;
-        vector<int>ans;
-        int n=arr.size();
-        while(e<n){
-            while(!mindq.empty() && arr[mindq.back()]>arr[e]) mindq.pop_back();
-            while(!maxdq.empty() &&arr[maxdq.back()]<arr[e]) maxdq.pop_back();
-            mindq.push_back(e);
-            maxdq.push_back(e);
-            
-            while(arr[maxdq.front()]-arr[mindq.front()]>x){
-                if(s==maxdq.front()) maxdq.pop_front();
-                if(s==mindq.front()) mindq.pop_front();
-                s++;
+        int n = arr.size();
+        int left = 0, maxLen = 0, startIdx = 0;
+        multiset<int> window;
+
+        for (int right = 0; right < n; ++right) {
+            window.insert(arr[right]);
+
+            // Shrink window if max - min > x
+            while (*window.rbegin() - *window.begin() > x) {
+                window.erase(window.find(arr[left]));
+                ++left;
             }
-            if(e-s>ansEnd-ansStart){
-                ansEnd=e;
-                ansStart=s;
+
+            // Update longest window
+            if (right - left + 1 > maxLen) {
+                maxLen = right - left + 1;
+                startIdx = left;
             }
-            e++;
         }
-        for(int i=ansStart;i<=ansEnd;i++){
-            ans.push_back(arr[i]);
-        }
-        return ans;
-        
+
+        return vector<int>(arr.begin() + startIdx, arr.begin() + startIdx + maxLen);
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.longestSubarray(arr, k);
-        for (auto it : ans) {
-            cout << it << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
